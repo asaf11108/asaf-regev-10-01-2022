bd39686f3477935f2595e64ac742e4f6a6d97f59
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2022-01-11 19:05:18 +0200

    validation

--- src/components/ControlledAutocomplete/ControlledAutocomplete.tsx
+++ src/components/ControlledAutocomplete/ControlledAutocomplete.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect } from 'react';
 import './ControlledAutocomplete.scss';
 import { useState } from 'react';
-import { Autocomplete, CircularProgress, debounce, InputAdornment, TextField } from '@mui/material';
+import { Autocomplete, debounce, FormControl, FormHelperText, TextField } from '@mui/material';
 import { getLocations } from '../../api/api.service';
 import { Location } from '../../interfaces/favorite-location.interface';
 import { ControlledAutocompleteProps } from './ControlledAutocomplete.model';
@@ -10,10 +10,17 @@ import SearchIcon from '@mui/icons-material/Search';
 const ControlledAutocomplete: React.FC<ControlledAutocompleteProps> = ({ selected, handleChange }) => {
   const [options, setOptions] = useState<Location[]>([]);
   const [inputValue, setInputValue] = React.useState(selected.localizedName);
+  const [valid, setValid] = React.useState(true);
+  const regex = /^[a-zA-Z ]+$/;
+  const isValid = (query: string): boolean => !!query.length && regex.test(query);
 
   const getOptionsDelayed = useCallback(
     debounce((query, callback) => {
-      getLocations(query).then(res => res.map(location => ({ key: location.Key, localizedName: location.LocalizedName }))).then(callback);
+      if (isValid(query)) {
+        getLocations(query).then(res => res.map(location => ({ key: location.Key, localizedName: location.LocalizedName }))).then(callback);
+      } else {
+        callback([]);
+      }
     }, 1000),
     []
   );
@@ -24,27 +31,35 @@ const ControlledAutocomplete: React.FC<ControlledAutocompleteProps> = ({ selecte
     });
   }, [inputValue, getOptionsDelayed]);
 
+  const handleInputChange = (query: string): void => {
+    setValid(isValid(query));
+    setInputValue(query);
+  }
+
   return (
     <div className='autocomplete'>
-      <Autocomplete
-        options={options}
-        getOptionLabel={(option) => option.localizedName}
-        defaultValue={selected}
-        onInputChange={(_, newInputValue) => setInputValue(newInputValue)}
-        onChange={(_, location) => location && handleChange(location.key)}
-        renderInput={(params) => 
-          <TextField
-            {...params}
-            variant="standard"
-            placeholder="Search location"
-            InputProps={{
-              ...params.InputProps,
-              startAdornment: (
-                <SearchIcon />
-              )
-            }}
-          />}
-      />
+      <FormControl>
+        <Autocomplete
+          options={options}
+          getOptionLabel={(option) => option.localizedName}
+          defaultValue={selected}
+          onInputChange={(_, newInputValue) => handleInputChange(newInputValue)}
+          onChange={(_, location) => location && handleChange(location.key)}
+          renderInput={(params) =>
+            <TextField
+              {...params}
+              variant="standard"
+              placeholder="Search location"
+              InputProps={{
+                ...params.InputProps,
+                startAdornment: (
+                  <SearchIcon />
+                )
+              }}
+            />}
+        />
+        {!valid && <FormHelperText error>Invalid input</FormHelperText>}
+      </FormControl>
 
     </div>
   );
