1bc9de44e24999853d2b4db8eedccac0aee38690
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2022-01-13 15:40:45 +0200

    interceptor arrow function

--- src/interceptors/http.interceptor.ts
+++ src/interceptors/http.interceptor.ts
@@ -5,18 +5,19 @@ const API_KEY = "gRf4KNnswLuVm8mG3puAI1GUOGeJTu1v";
 
 export function httpInterceptor(setOpen: Dispatch<SetStateAction<boolean>>): void {
     // Add a request interceptor
-    axios.interceptors.request.use(function (config) {
-        config.params = { ...config.params, apikey: API_KEY };
-        return config;
-    }, function (error) {
-        return Promise.reject(error);
-    });
+    axios.interceptors.request.use(
+        config => {
+            config.params = { ...config.params, apikey: API_KEY };
+            return config;
+        }
+    );
 
     // Add a response interceptor
-    axios.interceptors.response.use(function (response) {
-        return response;
-    }, function (error) {
-        setOpen(true);
-        return Promise.reject(error);
-    });
+    axios.interceptors.response.use(
+        x => x,
+        error => {
+            setOpen(true);
+            return Promise.reject(error);
+        }
+    );
 }
