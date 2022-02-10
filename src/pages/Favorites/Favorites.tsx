63f3804dbf55be2c0ea7fb45466ef436d5b8e9ea
asaf11108 <asaf11108@gmail.com>
2022-02-10 11:39:54 +0200

    transition one after another

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -38,8 +38,11 @@ const Favorites: FC = () => {
       <TransitionGroup className="favorites">
         {
           favoriteLocations.map((favoriteLocation, index) =>
-            <CSSTransition key={favoriteLocation.key} timeout={500 * (index + 1)} classNames="favorites__forecast" appear>
-              <div className="favorites__forecast" onClick={() => handleActiveForecast(favoriteLocation)}>
+            <CSSTransition key={favoriteLocation.key} timeout={300 * (index + 1)} classNames="favorites__forecast" appear>
+              <div
+                className="favorites__forecast" 
+                style={{transitionDelay: `${300 * index}ms`}}
+                onClick={() => handleActiveForecast(favoriteLocation)}>
                 <Forecast forecast={mapToForecastComponent(favoriteLocation)} />
               </div>
             </CSSTransition>
