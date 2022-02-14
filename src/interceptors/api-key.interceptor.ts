597a5f3a4c5be2c035bb55685757c1fdfd1763b1
asaf11108 <asaf11108@gmail.com>
2022-02-14 22:40:16 +0200

    unused var

--- src/interceptors/api-key.interceptor.ts
+++ src/interceptors/api-key.interceptor.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const API_KEY = "gRf4KNnswLuVm8mG3puAI1GUOGeJTu1v";
-
 const apiKeyInterceptor = (): void => {
     axios.interceptors.request.use(
         config => {
