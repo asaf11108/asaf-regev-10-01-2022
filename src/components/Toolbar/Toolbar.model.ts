d9fbfa4de0be4125cf6bd1638195d369644a360c
asaf11108 <asaf11108@gmail.com>
2022-02-14 23:12:34 +0200

    Delete src/components/Toolbar directory

--- src/components/Toolbar/Toolbar.model.ts
+++
@@ -1,7 +0,0 @@
-import { FavoriteLocation } from "../../store/favorite-locations/favorite-locations.model";
-
-export interface IMenuItem {
-    label: string;
-    navigatePath: string;
-    disabled?: (arg1: FavoriteLocation[]) => boolean;
-};
