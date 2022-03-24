086cc4b6727471a09e2c00cbddedc2a673103668
Asaf Regev <asaf.regev@shieldfc.com>
2022-03-24 15:20:06 +0200

    lint

--- src/pages/home/home.test.tsx
+++ src/pages/home/home.test.tsx
@@ -1,6 +1,19 @@
+import { fireEvent } from '@testing-library/react';
 import { renderWithProviders } from '../../testing/render-with-providers';
 import Home from './home';
 
-it('It should mount', () => {
-  renderWithProviders(<Home />);
-});
+describe('Home', () => {
+  it('should mount', () => {
+    renderWithProviders(<Home />);
+  });
+  
+  it('should mount different location', () => {
+    // renderWithProviders(<Home />);
+  });
+
+  it('should mark location as favorite', () => {
+    renderWithProviders(<Home />);
+
+    // fireEvent.click(screen.get)
+  });
+})
