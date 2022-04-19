2002d70cbf7567aaf6271b8b44e4feec8778d9b4
Asaf Regev <asaf.regev@shieldfc.com>
2022-04-19 21:56:26 +0300

    locale hook

--- src/components/table/columns.hook.tsx
+++ src/components/table/columns.hook.tsx
@@ -3,8 +3,11 @@ import Favorite from "../favorite/favorite";
 import { ColumnType, TableColumn, _TableColumn } from "./table.model";
 import { get } from "lodash-es";
 import { format } from "date-fns";
+import { useLocale } from "../../providers/locale/locale.hook";
 
 export const useColumns = (columns: TableColumn[]): _TableColumn[] => {
+    const locale = useLocale();
+
     return useMemo(() =>
         columns.map(col => {
             let cell: _TableColumn['cell'] = () => <></>;
@@ -14,10 +17,10 @@ export const useColumns = (columns: TableColumn[]): _TableColumn[] => {
                     cell = row => <span style={{ fontSize: '1.2em' }}><Favorite isFavorite={get(row, col.prop)} /></span>
                     break;
                 case ColumnType.Date:
-                    cell = row => <span>{format(new Date(get(row, col.prop)), 'P')}</span>
+                    cell = row => <span>{format(new Date(get(row, col.prop)), 'P', { locale })}</span>
                     break;
                 case ColumnType.LongDate:
-                    cell = row => <span>{format(new Date(get(row, col.prop)), 'Pp')}</span>
+                    cell = row => <span>{format(new Date(get(row, col.prop)), 'Pp', { locale })}</span>
                     break;
                 default:
                     cell = row => <span>{get(row, col.prop)}</span>
