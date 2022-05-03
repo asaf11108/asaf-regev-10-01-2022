4cfd366a7445518de3ea762705b2c746d35c0d5a
Asaf Regev <asaf.regev@shieldfc.com>
2022-05-03 22:55:14 +0300

    list entrance css prop

--- src/animations/list-entrance.tsx
+++ src/animations/list-entrance.tsx
@@ -1,21 +1,33 @@
-import { CSSProperties, FC } from "react";
+/** @jsxImportSource @emotion/react */
+import { SerializedStyles, css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { FC } from "react";
 import { Transition } from "react-transition-group";
+
+export interface ListEntranceProps {
+  index: number;
+  list?: any[];
+}
 
 export const ANIMATION_DELAY = 300;
 
-const transitionStyles: Record<string, CSSProperties> = {
-    entering: { opacity: 0, transform: 'scale(0.9)' },
-    entered:  { opacity: 1, transform: 'translateX(0)' },
-  };
+const Animation = styled.div`
+  transition-duration: ${ANIMATION_DELAY}ms;
+`;
 
-export const ListEntrance: FC<{ index: number }> = ({ index, children }) => (
-    <Transition in={true} timeout={ANIMATION_DELAY * (index + 1)} appear>
-      {state => (
-        <div style={{
-          ...transitionStyles[state]
-        }}>
-          {children}
-        </div>
-      )}
-    </Transition>
-  );
+const transitionStyles: Record<string, SerializedStyles> = {
+  entering: css`
+    opacity: 0;
+    transform: scale(0.9);
+  `,
+  entered: css`
+    opacity: 1;
+    transform: scale(1);
+  `,
+};
+
+export const ListEntrance: FC<ListEntranceProps> = ({ index, children }) => (
+  <Transition in={true} timeout={ANIMATION_DELAY * (index + 1)} appear>
+    {(state) => <Animation css={transitionStyles[state]}>{children}</Animation>}
+  </Transition>
+);
