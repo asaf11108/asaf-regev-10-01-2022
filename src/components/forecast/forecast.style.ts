4362d3140ec838102ce5e4df858dea9632c60a4e
Asaf Regev <asaf.regev@shieldfc.com>
2022-06-05 11:04:34 +0300

    forecast rem

--- src/components/forecast/forecast.style.ts
+++ src/components/forecast/forecast.style.ts
@@ -1,13 +1,13 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import { Card } from "@mui/material";
-import { em, rem } from "polished";
+import { rem } from "polished";
 import { spread } from "../../styles/vendors/spread";
 import { ForecastProps } from "./forecast.model";
 
 export const Forecast = styled(Card)<Pick<ForecastProps, 'onClick'>> `
     ${spread('width', rem('112px'), rem('152px'))}
-    ${spread('margin', em('4px'), em('16px'))}
+    ${spread('margin', rem('4px'), rem('16px'))}
     text-align: center;
     background-color: white;
 
