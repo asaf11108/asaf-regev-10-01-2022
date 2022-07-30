7a13bdc44d9bc59cf0bc142062f3e7cdc2911483
Asaf Regev <asaf.regev@shieldfc.com>
2022-07-30 11:27:16 +0300

    config svgr

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,15 @@
 const {useBabelRc, override} = require('customize-cra');
 
+const useSVGR = config => {
+    config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+    })
+    return config
+}
+
 module.exports = override(
-    useBabelRc()
+    useBabelRc(),
+    useSVGR
 );
