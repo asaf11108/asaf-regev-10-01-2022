2ac5531896712609e4e82961462fedd3f4427ea2
Asaf Regev <asaf.regev@shieldfc.com>
2022-12-20 15:45:51 +0200

    add favorite location effect

---
+++ src/store-elf/favorite-locations/favorite-locations.effect.ts
@@ -0,0 +1,50 @@
+import { setProp, setProps } from "@ngneat/elf";
+import { upsertEntities } from "@ngneat/elf-entities";
+import { format } from "date-fns";
+import API from "../../api/api";
+import { _Forecast } from "../../components/forecast/forecast.model";
+import { FavoriteLocation, Location } from '../../store/favorite-locations/favorite-locations.model';
+import { favoriteLocationsStore } from "./favorite-locations.state";
+import { createEffectFn } from '@ngneat/effects';
+import { forkJoin, Observable } from "rxjs";
+import { map, tap, switchMap } from "rxjs/operators";
+
+export const loadFavoriteLocation$ = createEffectFn((selectedOption$: Observable<Location>) => {
+    return selectedOption$.pipe(
+        tap(() => favoriteLocationsStore.update(setProp('loading', true))),
+        switchMap(selectedOption => forkJoin([
+            API.getCurrentConditions(selectedOption.key),
+            API.getForecasts(selectedOption.key)
+        ]).pipe(map(res => ({ selectedOption, res })))),
+        map(({ selectedOption, res }) => {
+            const currentConditions = res[0][0];
+            const forecasts: _Forecast[] = res[1].map(forecast => ({
+                title: format(new Date(forecast.Date), "EEE"),
+                temperature: `${forecast.Temperature.Minimum.Value}\u00B0C`,
+            }));
+    
+            return {
+                key: selectedOption.key,
+                localizedName: selectedOption.localizedName,
+                temperature: `${currentConditions.Temperature.Metric.Value}\u00B0C`,
+                weatherText: currentConditions.WeatherText,
+                icon: currentConditions.WeatherIcon.toString(),
+                forecasts,
+                searchedDate: new Date().toISOString()
+            } as FavoriteLocation;
+        }),
+        tap({
+            next: favoriteLocation => {
+                favoriteLocationsStore.update(
+                    upsertEntities([favoriteLocation]),
+                    setProps({ loading: false, error: null }),
+                );
+            },
+            error: () => {
+                favoriteLocationsStore.update(
+                    setProps({ loading: false, error: true }),
+                );
+            }
+        })
+    );
+})
--- src/store-elf/favorite-locations/favorite-locations.state.ts
+++ src/store-elf/favorite-locations/favorite-locations.state.ts
@@ -1,46 +1,12 @@
-import { createStore, setProp, setProps, withProps } from '@ngneat/elf';
-import { upsertEntities, withActiveId, withEntities } from '@ngneat/elf-entities';
-import { format } from 'date-fns';
-import API from '../../api/api';
+import { createStore, withProps } from '@ngneat/elf';
+import { withActiveId, withEntities } from '@ngneat/elf-entities';
 import { _Forecast } from '../../components/forecast/forecast.model';
-import { FavoriteLocation, Location } from '../../store/favorite-locations/favorite-locations.model';
+import { FavoriteLocation } from '../../store/favorite-locations/favorite-locations.model';
 
-const favoriteLocationsStore = createStore(
+export const favoriteLocationsStore = createStore(
   { name: 'favoriteLocation',  },
   withProps({ loading: false, error: null as any }),
   withEntities<FavoriteLocation, 'key'>({ idKey: 'key' }),
   withActiveId()
 );
 
-export function fetchFavoriteLocation(selectedOption: Location) {
-    favoriteLocationsStore.update(setProp('loading', true));
-    return Promise.all([
-        API.getCurrentConditions(selectedOption.key),
-        API.getForecasts(selectedOption.key)
-    ]).then(res => {
-        const currentConditions = res[0][0];
-        const forecasts: _Forecast[] = res[1].map(forecast => ({
-            title: format(new Date(forecast.Date), "EEE"),
-            temperature: `${forecast.Temperature.Minimum.Value}\u00B0C`,
-        }));
-
-        return {
-            key: selectedOption.key,
-            localizedName: selectedOption.localizedName,
-            temperature: `${currentConditions.Temperature.Metric.Value}\u00B0C`,
-            weatherText: currentConditions.WeatherText,
-            icon: currentConditions.WeatherIcon.toString(),
-            forecasts,
-            searchedDate: new Date().toISOString()
-        } as FavoriteLocation;
-    }).then(favoriteLocation => {
-        favoriteLocationsStore.update(
-            upsertEntities([favoriteLocation]),
-            setProps({ loading: false, error: null }),
-        );
-    }).catch(() => {
-        favoriteLocationsStore.update(
-            setProps({ loading: false, error: true }),
-        );
-    });
-}
