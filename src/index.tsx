dc44a8f4fdc0ddcfc269ce023d192352b7845bc1
Asaf Regev <asaf.regev@shieldfc.com>
2023-01-05 11:55:56 +0200

    react 18 render

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import ReactDOM from 'react-dom';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
 import { providersWrapper } from './providers-wrapper';
+import { createRoot } from "react-dom/client";
 
-
-ReactDOM.render(
-  providersWrapper(<App />),
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(providersWrapper(<App />));
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
